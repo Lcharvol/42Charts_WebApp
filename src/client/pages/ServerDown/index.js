fd204743daa8ed057426126d13666e666d195c8f
Lcharvol <charvolin0@gmail.com>
2018-11-06 20:37:56 +0100

    WIP

--- src/client/pages/ServerDown/index.js
+++ src/client/pages/ServerDown/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Redirect } from 'react-router';
 
-import { Container } from './styles';
+import { Container, Text, Content } from './styles';
 import Button from '../../components/Button';
 import { reqPing } from '../../requests';
 
 const ServerDown = ({ history }) => (
   <Container>
-    <Button
-      label={'Refresh'}
-      action={() => {
-        reqPing()
-          .then(res => window.location.replace('/'))
-          .catch(err => {
-            window.location.reload();
-          });
-      }}
-    />
+    <Content>
+      <Text>OUPS!</Text>
+      <Button
+        label={'Refresh'}
+        action={() => {
+          reqPing()
+            .then(res => window.location.replace('/'))
+            .catch(err => {
+              window.location.reload();
+            });
+        }}
+      />
+    </Content>
   </Container>
 );
 
--- src/client/pages/ServerDown/styles.js
+++ src/client/pages/ServerDown/styles.js
@@ -1,10 +1,28 @@
 import styled from 'styled-components';
+import { MAIN_COLOR } from '../../constants/colors';
 
 export const Container = styled.div`
   position: relative;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   min-width: 100vw;
   min-height: 100vh;
+`;
+
+export const Content = styled.div`
+  postion: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 500px;
+`;
+
+export const Text = styled.div`
+  position: relative;
+  display: flex;
+  color: ${MAIN_COLOR};
+  font-size: 1em;
 `;
