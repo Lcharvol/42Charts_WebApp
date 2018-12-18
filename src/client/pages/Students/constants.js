4be0ae1529422b2e96dfd66bd9c2b5e7937ba342
Lcharvol <lcharvol@student.42.fr>
2018-12-18 16:24:44 +0100

    Add campus filter

--- src/client/pages/Students/constants.js
+++ src/client/pages/Students/constants.js
@@ -12,3 +12,46 @@ export const FILTER_VALUES = [
     label: 'logtime',
   },
 ];
+
+export const CAMPUS_FILTER_VALUES = [
+  {
+    id: 0,
+    label: 'all',
+  },
+  {
+    id: 1,
+    label: 'Paris',
+  },
+  {
+    id: 2,
+    label: 'Kyiv',
+  },
+  {
+    id: 3,
+    label: 'Brussels',
+  },
+  {
+    id: 4,
+    label: 'Fremont',
+  },
+  {
+    id: 5,
+    label: 'Lyon',
+  },
+  {
+    id: 6,
+    label: 'Montrouge',
+  },
+  {
+    id: 7,
+    label: 'Johannesburg',
+  },
+  {
+    id: 8,
+    label: 'Cluj',
+  },
+  {
+    id: 8,
+    label: 'Chisinau',
+  },
+];
